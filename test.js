44dfa8a01853ebf8d31bf7177f9a89b944f3bb9c
manojkumar-vemula <118467867+manojkumar-vemula@users.noreply.github.com>
2023-02-22 18:37:47 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 const sideMenu = document.querySelector("aside");
 const menuBtn = document.querySelector("#menu-btn");
 const closeBtn = document.querySelector("#close-btn");
-const themeToggler = document.querySelector(".theme-Toggler");
+const themeToggler = document.querySelector(".theme-toggler");
 // show sidebar
 menuBtn.addEventListener('click',() => {
     sideMenu.style.display = 'block';
